e87b82593ef8fa0e23e04cb08f1fe269474fd67a
ItsGabe415 <galopez2@s.sfusd.edu>
2025-11-30 20:14:52 -0800

    M8L5 done

---
+++ lessons/slideshow_entry.gd
@@ -0,0 +1,7 @@
+class_name SlideShowEntry extends Resource
+
+
+
+@export var expression := preload("res://assets/emotion_regular.png")
+@export var character := preload("res://assets/sophia.png")
+@export_multiline var text := ""
